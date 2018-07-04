3e8aa2fc094d57e44282f77a051a9eaa848efc92
aliyun-k <22542812@qq.com>
2018-07-04 19:10:33 +0800

    tcp

--- tcp/tcp.h
+++ tcp/tcp.h
@@ -2,15 +2,30 @@
 *
 *
 */
+#include <string>
+#include <iostream>
 
+namespace TCP{
 
-
-namespace TCP {
-
-class TCP(){
+class TCP_C {
   public:
-
-
-
+      TCP_C(std::string addr,int port);
+      int Send(void *);
+      int Recv(void *){};
+  private:
+      int socket_fd;
 };
+
+/*
+class Tcp_S : public Tcp {
+  public:
+          TCP_S(    );
+      int Send(void *){};
+      int Recv(void *){};
+  private:
+      int Init()=0;
+};
+
+*/
+
 }// namespace end
